0837e9de1bcacb569b0710ec7436b63e0fa63904
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-19 12:33:17 -0600

    connected sandwhich and dom, sumbit button works

---
+++ javascripts/DOMinteraction.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let submit = document.getElementById("submit");
+submit.addEventListener("click", () =>{
+    console.log("button clicked");
+});
+
+let output = document.getElementById("output");
